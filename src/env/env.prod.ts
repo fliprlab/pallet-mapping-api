8d53b6b396f32281d18f0f314085b5ea22be1fd2
ajay2966 <ajmadhpuriya1234@gmail.com>
2023-04-18 13:30:14 +0530

    update env and update package.json and and create shipment query

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,7 +1,7 @@
 class EnvProd implements EnvironmentVariable {
   baseUrl: string = "localhost";
   mondoDbUrl: string =
-    "mongodb+srv://pallet:Pallet%401640@pallet-mapping.pzwkr8h.mongodb.net/live";
+    "mongodb+srv://pallet:Pallet%401640@pallet-mapping.pzwkr8h.mongodb.net/prod";
   jwtSecret: string =
     "$2a$10$YKnL4ZCPU2iJpnmNdtWxtO1YWw8AWg48ESn2zx.DLOZ3pV62soZdS";
   headerKey: string = "authorization";
