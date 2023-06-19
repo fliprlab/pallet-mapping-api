bcd56c825acf9a94a381144f40445ff497a1db9f
Rajveer jatav <87698946+Rajveerjatav@users.noreply.github.com>
2023-06-19 19:24:10 +0530

    update
    update

--- src/routes/hub/queries/grid.query.ts
+++ src/routes/hub/queries/grid.query.ts
@@ -3,4 +3,5 @@ import { gridController } from "../../../controllers/grid.controller";
 
 export const gridQueries = (router: Router) => {
   router.get("/get-grids", gridController.getGrids);
+  router.post("/add-grids", gridController.addMultipleGrids);
 };
