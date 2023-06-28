ea750c36f00184de41adb38d215ceae0ae89884e
Abhay jatav <abhayjatav777@gmail.com>
2023-06-28 17:38:03 +0530

    added new url for admin

--- src/app.ts
+++ src/app.ts
@@ -70,6 +70,7 @@ app.use(
       "https://pmapping-web-v2.dev.flipr.co.in", // Dev
       "http://localhost:3000",
       "http://localhost:3001",
+      "https://pmapping-admin.dev.flipr.co.in/",
     ],
   })
 );
